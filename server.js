90ccb8ecb09c1ac235bf0f26c855225d0523f312
takuma ohno <t_km@hotmail.co.jp>
2013-06-08 11:35:28 +0900

    commit

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 var http = require('http');
 http.createServer(function (req, res) {
   res.writeHead(200, {'Content-Type': 'text/plain'});
-  res.end('Hello World!\n');
+  res.end('Hello World\n');
   console.log('request url: ', req.url);
 }).listen(80);
